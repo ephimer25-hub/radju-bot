31b34339770daa7c9a8bfc30c319c9bb8992c3b4
ephimer25-hub <ephimer25@gmail.com>
2026-09-12 17:26:45 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from threading import Thread
 import base64
 
 # --- НАСТРОЙКИ И ТОКЕНЫ ---
-TELEGRAM_TOKEN = "8804377859:AAFxUkT63bRc05ORljg9VuDbG-L_GkF3OEI"  # Этот токен оставляем прежним, который от BotFather
+TELEGRAM_TOKEN = "8804377859:AAHybYDorTb9c4j-o90D2tHYr_x2NNo0qaE"  # Этот токен оставляем прежним, который от BotFather
 AITUNNEL_TOKEN = "sk-w7178jSfKD6ttClL6jKlJ67BPKfJDmZL"  # Вставляем сюда ключ, который начинается на pk-
 
 # Меняем старый адрес ИИ-Туннеля на прямой адрес ProxyAPI
